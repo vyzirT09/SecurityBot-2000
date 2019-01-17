5ad56a7984527ddfa59590a973bd61b4752046a8
vyzirT09 <johntd090@gmail.com>
2019-01-17 15:36:52 -0800

    updated info to have link here

--- commands/info.js
+++ commands/info.js
@@ -15,6 +15,7 @@ exports.run = (client, message, args, sql, Discord, config) =>
         .addField(`About`, `SecurityBot was made in Microsoft Visual Studio Code using node.js and discord.js. The discord.js library can be found [here](https://discord.js.org/#/docs/main/stable/general/welcome).` +
         ` He is currently hosted on the dev's computer, but the dev is looking for a permanent, stable host.`)
         .addField(`Support`, `If you're having an issue with SecurityBot, feel free to DM the owner, ${owner}. They will address it as soon as they possibly can.`)
+        .addField(`GitHub`, `SecurityBot 2000's code can be found on GitHub [here](https://github.com/vyzirT09/SecurityBot-2000).`)
         .addField(`Meet the family!`, `Come join the conversation at [MemeBot HQ](https://discord.gg/w2PrTYb)! Here you can chat with the dev, recieve updates on SecurityBot, and meet the rest of his bot family!`)
         .setTimestamp()
         .setFooter(`SecurityBot 2000's profile picture drawn by Lofi.`);
