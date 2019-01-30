b42214700d1ffbd8181ca24acce8baf4bdf97fa4
vyzirT09 <johntd090@gmail.com>
2019-01-29 23:36:23 -0800

    whoopsie forgot to push this

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args, sql, Discord, config) =>
 {
     message.channel.send(`Add SecurityBot 2000 to your server by clicking this link: https://discordapp.com/oauth2/authorize?client_id=413521470317264896&scope=bot&permissions=268443710.\n` +
-                         `You can disable the permissions while adding it, although the moderator features may not work if you do.`)
+                         `You can disable the permissions while adding it, although the moderator features may not work if you do.`);
 }
