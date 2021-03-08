718d01e18cc77493574f6040bea25aeaaf999868
16 16 <71988762+blak11@users.noreply.github.com>
2021-03-08 20:17:58 +0300

    Update banhammer.js

--- commands/banhammer.js
+++ commands/banhammer.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args, sql, Discord, config) =>
 {
-    message.channel.send('https://cdn.discordapp.com/attachments/284163726423228418/326898344779841536/banhammer.jpg'); //Just for fun
+    message.channel.send('https://cdn.dicordapp.com/attachments/284163726423228418/326898344779841536/banhammer.jpg'); //Just for fun
 }
